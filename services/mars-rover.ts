41689db45da745fae53f1a16f37a43fbc5a4ed66
londonbarrett <alondono.dev@gmail.com>
2023-09-04 15:55:41 -0500

    Fixes service url

--- services/mars-rover.ts
+++ services/mars-rover.ts
@@ -2,7 +2,7 @@ import { Photo, SearchParams } from "@/types";
 
 export const getLatestPhotos = async (): Promise<Photo[]> => {
   const response = await fetch(
-    "https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/latest_photos?api_key=Ss0PHYBzSfal9L0xXhzYHOVId9agg0HeGf6BOpBM",
+    "https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/latest_photos",
     { cache: "reload" }
   );
   const data = await response.json();
